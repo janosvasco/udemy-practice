f0891ae5e3fb79849b3266adc4c3dded5bfbf3af
Sandor Janos Vasco <janosvasco@gmail.com>
2023-10-23 14:08:45 +0200

    creating formdata comp with states

---
+++ Projects/financial calc/src/components/Formdata.js
@@ -0,0 +1,56 @@
+import FormButtons from "./FormButtons";
+import FormInput from "./FormInput";
+import { useState } from "react";
+
+export default function Formdata(props) {
+  const [currSavings, setCurrSavings] = useState(1000);
+  const [yearlySavings, setYearlySavings] = useState(100);
+  const [expInterest, setExpInterest] = useState(10);
+  const [duration, setDuration] = useState(5);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onSave({ currSavings, yearlySavings, expInterest, duration });
+  }
+
+  return (
+    <form className="form" onSubmit={submitHandler}>
+      <div className="input-group">
+        <FormInput
+          id="current-savings"
+          label="Current Savings ($)"
+          onChangeHandler={(e) => setCurrSavings(e.target.value)}
+          value={currSavings}
+        />
+        <FormInput
+          id="yearly-contribution"
+          label="Yearly Savings ($)"
+          onChangeHandler={(e) => setYearlySavings(e.target.value)}
+          value={yearlySavings}
+        />
+      </div>
+      <div className="input-group">
+        <FormInput
+          id="expected-return"
+          label="Expected Interest (%, per year)"
+          onChangeHandler={(e) => setExpInterest(e.target.value)}
+          value={expInterest}
+        />
+        <FormInput
+          id="duration"
+          label="Investment Duration (years)"
+          onChangeHandler={(e) => setDuration(e.target.value)}
+          value={duration}
+        />
+      </div>
+      <FormButtons
+        onClickHandler={{
+          setCurrSavings,
+          setYearlySavings,
+          setExpInterest,
+          setDuration,
+        }}
+      />
+    </form>
+  );
+}
