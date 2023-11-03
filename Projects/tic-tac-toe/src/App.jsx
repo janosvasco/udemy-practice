73d1e91d5c7210451ccf3ca188d9f40418b7942a
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-03 16:27:23 +0100

    removing unnecessary state

--- Projects/tic-tac-toe/src/App.jsx
+++ Projects/tic-tac-toe/src/App.jsx
@@ -3,19 +3,22 @@ import GambeBoard from "./components/GambeBoard";
 import Player from "./components/Player";
 import Log from "./Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currActivePlayer) =>
-      currActivePlayer === "X" ? "O" : "X"
-    );
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      let currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
