3a850ac02877402ecd6996ca06d808518f6b8c3e
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-20 21:40:36 +0100

    user Input via Refs

--- Projects/forms/src/components/Login.jsx
+++ Projects/forms/src/components/Login.jsx
@@ -1,17 +1,48 @@
+import { useRef, useState } from "react";
+
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
+  const email = useRef();
+  const password = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    //console.log(enteredEmail, enteredPassword);
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("sending http request");
+
+    //email.current.value = ""; => reseting the form
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email adress.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
