079daa2ba6c2a9c42c308eeaf93cb37fcf51dcb7
Sandor Janos Vasco <janosvasco@gmail.com>
2023-12-10 20:35:58 +0100

    rendering EventItem comp, making loader

--- Projects/advanced-router/frontend/src/pages/EventDetail.js
+++ Projects/advanced-router/frontend/src/pages/EventDetail.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
   return (
     <>
-      <h1>Event Detail Page</h1>
-      <p>Selected event: {params.id}</p>
+      <EventItem event={data.event} />
     </>
   );
+}
+
+export async function loader({ request, params }) {
+  const id = params.id;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event" },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
 }
