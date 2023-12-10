4494dfc23e5ad0a676e902bd303cc96625c3569e
Sandor Janos Vasco <janosvasco@gmail.com>
2023-12-10 20:32:46 +0100

    creating error handling page

--- Projects/advanced-router/frontend/src/pages/Error.js
+++ Projects/advanced-router/frontend/src/pages/Error.js
@@ -1,5 +1,29 @@
 import React from "react";
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 
 export default function Error() {
-  return <h1>An error occured!</h1>;
+  const error = useRouteError();
+
+  let title = "An error occured";
+  let message = "Something went wrong";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found";
+    message = "Could not find resource or page";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
