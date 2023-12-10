1c4000d5348aadf2bdfa2352b8c678b944c11530
Sandor Janos Vasco <janosvasco@gmail.com>
2023-12-10 20:30:54 +0100

    importing loaders

--- Projects/advanced-router/frontend/src/App.js
+++ Projects/advanced-router/frontend/src/App.js
@@ -22,13 +22,14 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import Root from "./pages/Root";
 import EventsRoot from "./pages/EventsRoot";
-import { loader as eventsLoader } from "./pages/Events";
 import Error from "./pages/Error";
 
 const router = createBrowserRouter([
@@ -53,6 +54,7 @@ const router = createBrowserRouter([
           {
             path: ":id",
             element: <EventDetailPage />,
+            loader: eventDetailLoader,
           },
           {
             path: "new",
