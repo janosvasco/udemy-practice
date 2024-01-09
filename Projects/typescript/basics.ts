3f8ea46e99a6fae94a53e88e019c41e149564f2d
Sandor Janos Vasco <janosvasco@gmail.com>
2024-01-09 17:43:58 +0100

    array&object types

--- Projects/typescript/basics.ts
+++ Projects/typescript/basics.ts
@@ -13,4 +13,29 @@ let userName: string;
 userName = "Max";
 
 let isInstructor: boolean;
+
 isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+/* person = { isEmployee: true };
+ */
+
+let people: {
+  name: "Max";
+  age: 32;
+}[];
